ce4d6ae9be6a5e8567552e074ea9716276f2d5d9
Igor Fesenko <Ky7m@users.noreply.github.com>
2017-10-26 10:52:08 +0300

    Add additional examples for string interpolation

---
+++ CSharpInternals/CSharpInternals.Strings/StringBasics.cs
@@ -0,0 +1,29 @@
+﻿using CSharpInternals.Utils;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpInternals.Strings
+{
+    public class StringBasics : BaseTestHelpersClass
+    {
+        private const string Emojis = "👶👶👶👶🍼🍼";
+
+        public StringBasics(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void StringLength()
+        {
+            //what do you think will be written?
+            WriteLine($"Length: {Emojis.Length}");    
+        }
+        
+        [Fact]
+        public void StringLengthInTextElements()
+        {
+            var stringInfo = new System.Globalization.StringInfo(Emojis);
+            WriteLine($"LengthInTextElements: {stringInfo.LengthInTextElements}");
+        }
+    }
+}
--- CSharpInternals/CSharpInternals.Strings/StringInterpolation.cs
+++ CSharpInternals/CSharpInternals.Strings/StringInterpolation.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using CSharpInternals.Utils;
@@ -21,6 +23,56 @@ namespace CSharpInternals.Strings
                 WriteLine("Hello from there! Dowloading information...");
                 return await new WebClient().DownloadStringTaskAsync("https://www.ifesenko.com/robots.txt");
             }))().GetAwaiter().GetResult()}!");
+        }
+
+        [Fact]
+        public void SqlCommand()
+        {
+            var name = "Igor'; DROP TABLE Users;--";
+            using (var connection = new SqlConnection())
+            {
+                using (var command = new SqlCommand($"SELECT * FROM Users WHERE Name='{name}'", connection))
+                {
+                    WriteLine(command.CommandText);
+                }
+            }
+        }
+        
+        [Fact]
+        public void NewSqlCommand()
+        {
+            var name = "Igor'; DROP TABLE Users;--";
+            using (var connection = new SqlConnection())
+            {
+                using (var command = connection.NewSqlCommand($"SELECT * FROM Users WHERE Name='{name}'"))
+                {
+                    WriteLine(command.CommandText);
+                    var p = command.Parameters[0];
+                    WriteLine($"{p.ParameterName}: Type={p.SqlDbType}; Value={p.Value}");
+                }
+            }
+        }
+        
+        
+    }
+    
+    // Do not use this code in production!
+    public static class SqlFormattableString
+    {
+        public static SqlCommand NewSqlCommand(this SqlConnection conn, FormattableString formattableString)
+        {
+            var sqlParameters = formattableString.GetArguments()
+                .Select((value, position) =>
+                    new SqlParameter(FormattableString.Invariant($"@p{position}"), value))
+                .ToArray();
+            
+            var formatArguments = sqlParameters.Select(p => p.ParameterName).ToArray();
+            
+            var sql = string.Format(formattableString.Format, formatArguments);
+            
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddRange(sqlParameters);
+            return command;
         }
     }
 }
