c8e4a575af7fb6712f09ecd42ba529af62043c83
Igor Fesenko <Ky7m@users.noreply.github.com>
2018-04-13 11:12:22 +0300

    Add UnderstandingAsync project

---
+++ AsynchronousAndMultithreading/Asynchronous/UnderstandingAsync/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UnderstandingAsync
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var stringData = await GetDataFromRemoteService();
+            await Console.Out.WriteLineAsync(stringData);
+        }
+
+        private static async Task<string> GetDataFromRemoteService()
+        {
+            const string url = "https://www.ifesenko.com/humans.txt";
+
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(url))
+            using (var content = response.Content)
+            {
+                var data = await content.ReadAsStringAsync();
+                return data ?? string.Empty;
+            }
+        }
+    }
+}
