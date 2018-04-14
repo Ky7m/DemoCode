4f4e16226ef63cf7c2c4721e0dbacf16a6fa39fe
Igor Fesenko <Ky7m@users.noreply.github.com>
2018-04-14 13:42:47 +0300

    Add IDisposable case

---
+++ AsynchronousAndMultithreading/Asynchronous/AsyncGotchas/DisposableService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncGotchas
+{
+    internal class DisposableService : IDisposable
+    {
+        public DisposableService()
+        {
+            Console.WriteLine($"{nameof(DisposableService)}.ctor");
+        }
+        
+        public async Task CalculationAsync(int a, int b)
+        {
+            await Task.Delay(a + b);
+            Console.WriteLine($"{nameof(DisposableService)}.{nameof(CalculationAsync)}");
+        }
+
+        private void Dispose(bool disposing)
+        {
+            var caller = disposing ? "dispose method" : "finalizer";
+            Console.WriteLine($"{nameof(DisposableService)}.Dispose called from {caller}");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~DisposableService()
+        {
+            Dispose(false);
+        }
+    }
+}
--- AsynchronousAndMultithreading/Asynchronous/AsyncGotchas/Program.cs
+++ AsynchronousAndMultithreading/Asynchronous/AsyncGotchas/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
 
 namespace AsyncGotchas
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine($"{nameof(DoWork)}.Started");
+            await DoWork();
+            WriteLine($"{nameof(DoWork)}.Finished");
+
+            WriteLine();
+            
+            WriteLine($"{nameof(DoWork)}.Started");
+            await DoWorkWithAwait();
+            WriteLine($"{nameof(DoWork)}.Finished");
         }
+
+        private static Task DoWork()
+        {
+            using (var service = new DisposableService())
+            {
+                var a = ComputeArg();
+                var b = ComputeArg();
+                return service.CalculationAsync(a, b);
+            }
+        }
+
+        private static async Task DoWorkWithAwait()
+        {
+            using (var service = new DisposableService())
+            {
+                var a = ComputeArg();
+                var b = ComputeArg();
+                await service.CalculationAsync(a, b);
+            }
+        }
+
+        private static int ComputeArg() => new Random().Next(500,1000);
     }
 }
