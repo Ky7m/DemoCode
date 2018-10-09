8af1b97607b521c82e36fb583732eb8f34cc678a
Igor Fesenko <ifesen@softserveinc.com>
2018-10-09 12:53:22 -0700

    add sample of team tab implementation

---
+++ MicrosoftTeamsApps/Microsoft.Teams.Tabs/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Microsoft.Teams.Tabs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .Configure(app => app.UseDefaultFiles().UseStaticFiles())
+                .Build()
+                .Run();
+        }
+    }
+}
