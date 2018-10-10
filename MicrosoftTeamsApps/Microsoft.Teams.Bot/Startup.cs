4b3fecccd415ac67872b6283e751e310c0636d68
Igor Fesenko <ifesen@softserveinc.com>
2018-10-10 10:48:25 -0700

    Add bot sample

---
+++ MicrosoftTeamsApps/Microsoft.Teams.Bot/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Teams.Bot
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBot<WelcomeUserBot>(options =>
+            {
+                options.CredentialProvider = new SimpleCredentialProvider(
+                    _configuration.GetValue<string>("AppId"), 
+                    _configuration.GetValue<string>("AppPassword"));
+
+                // Catches any errors that occur during a conversation turn and logs them.
+                options.OnTurnError = async (context, exception) =>
+                {
+                    await context.SendActivityAsync("Sorry, it looks like something went wrong.");
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}
