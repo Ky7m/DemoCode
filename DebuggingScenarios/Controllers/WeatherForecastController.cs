5fecc55bc46b9fc9edaf08f6cb7c8e15c52f3fb3
Igor Fesenko <ifesen@softserveinc.com>
2020-11-26 11:47:12 -0800

    add more complex scenario for Linq

--- DebuggingScenarios/Controllers/WeatherForecastController.cs
+++ DebuggingScenarios/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -15,24 +16,68 @@ namespace DebuggingScenarios.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            
+            int[] vectorA = { 0, 2, 4, 5, 6 };
+            int[] vectorB = { 1, 3, 5, 7, 8 };
+
+            var dotProduct = vectorA.Combine(vectorB, (a, b) => a * b).Sum();
+
+            if (dotProduct > 100 && rng.Next(-10, 10) > 0)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                dotProduct = rng.Next(-10, 10);
+            }
+            
+            return Enumerable.Range(1, 30)
+                .LogLinq("source", x => x.ToString())
+                .Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .LogLinq("forecast", x=> x.Summary)
+                .Where(x => x.Date <= DateTime.Now.AddDays(20))
+                .LogLinq("filtered", x=> x.Summary)
+                .Take(5)
+                .LogLinq("limited", x=> x.Summary)
+                .ToArray();
+        }
+    }
+
+    public static class SequenceExtensions
+    {
+        public static IEnumerable<T> Combine<T>(this IEnumerable<T> first, IEnumerable<T> second, Func<T, T, T> func)
+        {
+            using (IEnumerator<T> e1 = first.GetEnumerator(), e2 = second.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext())
+                {
+                    yield return func(e1.Current, e2.Current);
+                }
+            }
+        }
+        
+        public static IEnumerable<T> LogLinq<T>(this IEnumerable<T> enumerable, string logName, Func<T, string> printMethod)
+        {
+#if DEBUG
+            int count = 0;
+            foreach (var item in enumerable)
+            {
+                if (printMethod != null)
+                {
+                    Debug.WriteLine($"{logName}|item {count} = {printMethod(item)}");
+                }
+                count++;
+                yield return item;
+            }
+            Debug.WriteLine($"{logName}|count = {count}");
+#else   
+    return enumerable;
+#endif
         }
     }
 }
