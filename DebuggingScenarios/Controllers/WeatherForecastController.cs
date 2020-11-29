62d18299390abb9c4377b723453d67d4e73d0d24
Igor Fesenko <ifesen@softserveinc.com>
2020-11-29 03:02:23 +0000

    code clean up

--- DebuggingScenarios/Controllers/WeatherForecastController.cs
+++ DebuggingScenarios/Controllers/WeatherForecastController.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace DebuggingScenarios.Controllers
 {
