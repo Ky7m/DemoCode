ebc9c345263762b1d167342e45ba7d9a16bbf1a2
Igor Fesenko <ifesen@softserveinc.com>
2020-11-28 18:47:17 -0800

    add debugger specific attributes

--- DebuggingScenarios/WeatherForecast.cs
+++ DebuggingScenarios/WeatherForecast.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Diagnostics;
 
+// [assembly: DebuggerDisplay("Date: {Date,d,nq} | Summary: {Summary,nq}", Target = typeof(WeatherForecast))]
 namespace DebuggingScenarios
 {
+    [DebuggerDisplay("Date: {Date,d,nq} | Summary: {Summary,nq}")]
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
