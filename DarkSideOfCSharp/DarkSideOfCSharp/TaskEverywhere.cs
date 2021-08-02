024f98f696cd9a2547614fef367aee14b3b39300
Igor Fesenko <ifesen@softserveinc.com>
2021-08-02 14:41:56 -0700

    add a new project

---
+++ DarkSideOfCSharp/DarkSideOfCSharp/TaskEverywhere.cs
@@ -0,0 +1,93 @@
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.dotMemoryUnit;
+using WorkflowCore.Interface;
+using Xunit;
+
+namespace DarkSideOfCSharp
+{
+    public sealed class TaskEverywhere
+    {
+        [Fact]
+        [AssertTraffic(AllocatedObjectsCount = 1, Types = new[] {typeof(Task<string?>)})]
+        public async Task InMemoryQueueProvider()
+        {
+            var queueProvider = new InMemoryQueueProvider();
+            var i = 1000;
+
+            while (i --> 0)
+            {
+                var id = await queueProvider.DequeueWork(QueueType.Workflow, CancellationToken.None);
+            }
+        }
+    }
+    
+    
+    public sealed class InMemoryQueueProvider : IQueueProvider
+    {
+        private readonly ConcurrentQueue<string> _workflows = new();
+        private readonly ConcurrentQueue<string> _events = new();
+        private readonly ConcurrentQueue<string> _indices = new();
+        private readonly Task<string?> _nullValue = Task.FromResult<string?>(null);
+
+        public bool IsDequeueBlocking => true;
+
+        public Task QueueWork(string id, QueueType queue)
+        {
+            switch (queue)
+            {
+                case QueueType.Workflow:
+                    _workflows.Enqueue(id);
+                    break;
+                case QueueType.Event:
+                    _events.Enqueue(id);
+                    break;
+                case QueueType.Index:
+                    _indices.Enqueue(id);
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<string?> DequeueWork(QueueType queue, CancellationToken cancellationToken)
+        {
+            string? id = null;
+            switch (queue)
+            {
+                case QueueType.Workflow:
+                    _workflows.TryDequeue(out id);
+                    break;
+                case QueueType.Event:
+                    _events.TryDequeue(out id);
+                    break;
+                case QueueType.Index:
+                    _indices.TryDequeue(out id);
+                    break;
+            }
+
+            // if (id is null)
+            // {
+            //     return _nullValue;
+            // }
+
+            return Task.FromResult<string?>(id);
+        }
+
+        public Task Start()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Stop()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+    
+}
