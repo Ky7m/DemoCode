b06d8c848e5fe13e56ba7144f06fa3c94e31d58d
Igor Fesenko <ifesen@softserveinc.com>
2021-08-03 20:56:02 -0700

    add RecyclableMemoryStreamBenchmarks

---
+++ DarkSideOfCSharp/DarkSideOfCSharp/2.RecyclableMemoryStreamBenchmarks.cs
@@ -0,0 +1,97 @@
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using Microsoft.IO;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace DarkSideOfCSharp
+{
+    public class RecyclableMemoryStreamBenchmarks
+    {
+        private readonly WebhookResponse<CountryIndicator> _webhookResponse = new ()
+        {
+            Data = Enumerable.Range(1, 3000)
+                .Select(i => new CountryIndicator($"Country #{i}",$"Indicator #{i}", i))
+                .ToArray()
+        };
+        private readonly RecyclableMemoryStreamManager _memoryStreamManager = new ();
+        private readonly JsonSerializer _jsonSerializer = new();
+        private static readonly byte[] HmacKey = Encoding.Unicode.GetBytes(".NET fwdays'21 conference");
+
+        [Fact]
+        public void AssertThatAllMethodsProduceSameResult()
+        {
+            var expected = SerializeAndSign();
+            Assert.Equal(expected,SerializeAndSignUsingMemoryStream());
+            Assert.Equal(expected,SerializeAndSignUsingMemoryStream());
+        }
+        
+        [Benchmark(Baseline = true)]
+        public byte[] SerializeAndSign()
+        {
+            var widgetEnvJson = JsonConvert.SerializeObject(_webhookResponse);
+            var jsonBytes = Encoding.UTF8.GetBytes(widgetEnvJson);
+            using var hmac = new HMACSHA256(HmacKey);
+            return hmac.ComputeHash(jsonBytes);
+        }
+        
+        [Benchmark]
+        public byte[] SerializeAndSignUsingSystemTextJson()
+        {
+            var jsonBytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(_webhookResponse);
+            using var hmac = new HMACSHA256(HmacKey);
+            return hmac.ComputeHash(jsonBytes);
+        }
+
+        [Benchmark]
+        public byte[] SerializeAndSignUsingMemoryStream()
+        {
+            using var jsonStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(jsonStream, leaveOpen: true))
+            {
+                _jsonSerializer.Serialize(streamWriter, _webhookResponse);
+            }
+            
+            jsonStream.Position = 0;
+
+            using var hmac = new HMACSHA256(HmacKey);
+            return hmac.ComputeHash(jsonStream);
+        }
+        
+        [Benchmark]
+        public byte[] SerializeAndSignUsingRecyclableMemoryStream()
+        {
+            using var jsonStream = _memoryStreamManager.GetStream();
+            using (var streamWriter = new StreamWriter(jsonStream, leaveOpen: true))
+            {
+                _jsonSerializer.Serialize(streamWriter, _webhookResponse);
+            }
+                
+            jsonStream.Position = 0;
+
+            using var hmac = new HMACSHA256(HmacKey);
+            return hmac.ComputeHash(jsonStream);
+        }
+
+        private sealed class WebhookResponse<T>
+        {
+            public T[]? Data { get; set; }
+        }
+        private sealed class CountryIndicator
+        {
+            public CountryIndicator(string country, string indicator, int value)
+            {
+                Country = country;
+                Indicator = indicator;
+                Value = value;
+            }
+
+            public string Country { get; set; }
+            public string Indicator { get; set; }
+            public int Value { get; set; }
+        }
+    }
+}
