249d22261c01d4a211043cc5520613aac8551f6c
Igor Fesenko <Ky7m@users.noreply.github.com>
2022-05-27 03:15:33 +0000

    remove Wire due to security issue

--- MakingDotNETApplicationsFaster/MakingDotNETApplicationsFaster/Runners/SerializersPerformanceRunner.cs
+++ MakingDotNETApplicationsFaster/MakingDotNETApplicationsFaster/Runners/SerializersPerformanceRunner.cs
@@ -2,7 +2,6 @@ using System.IO;
 using BenchmarkDotNet.Attributes;
 using MakingDotNETApplicationsFaster.Runners.Models;
 using Newtonsoft.Json;
-using Wire;
 using ZeroFormatter;
 
 namespace MakingDotNETApplicationsFaster.Runners
@@ -11,15 +10,12 @@ namespace MakingDotNETApplicationsFaster.Runners
     public class SerializersPerformanceRunner
     {
         private readonly Summary _summary;
-        private readonly Serializer _wireSerializer = new Serializer(new SerializerOptions(true));
         private readonly string _summaryJson;
-        private readonly byte[] _summaryBytes;
         private readonly byte[] _summaryZeroFormatterBytes;
         public SerializersPerformanceRunner()
         {
             _summary = new Summary();
             _summaryJson = NewtonsoftJsonSerialization();
-            _summaryBytes = WireBinarySerialization();
             _summaryZeroFormatterBytes = ZeroFormatterBinarySerialization();
         }
 
@@ -27,16 +23,6 @@ namespace MakingDotNETApplicationsFaster.Runners
         public string NewtonsoftJsonSerialization()
         {
             return JsonConvert.SerializeObject(_summary);
-        }
-
-        [Benchmark]
-        public byte[] WireBinarySerialization()
-        {
-            using (var memoryStream = new MemoryStream())
-            {
-                _wireSerializer.Serialize(_summary, memoryStream);
-                return  memoryStream.ToArray();
-            }
         }
 
         [Benchmark]
@@ -49,17 +35,6 @@ namespace MakingDotNETApplicationsFaster.Runners
         public Summary NewtonsoftJsonDeserializer()
         {
             return JsonConvert.DeserializeObject<Summary>(_summaryJson);
-        }
-
-        [Benchmark]
-        public Summary WireBinaryDeserializer()
-        {
-            using (var memoryStream = new MemoryStream())
-            {
-                memoryStream.Write(_summaryBytes, 0, _summaryBytes.Length);
-                memoryStream.Position = 0;
-                return _wireSerializer.Deserialize<Summary>(memoryStream);
-            }
         }
 
         [Benchmark]
