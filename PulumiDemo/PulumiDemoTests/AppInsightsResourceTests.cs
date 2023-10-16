9b195c5145a2b446d50c896722a302d779ac0ef6
Igor Fesenko <Ky7m@users.noreply.github.com>
2023-10-15 21:56:55 -0700

    fix pulumi demo tests

--- PulumiDemo/PulumiDemoTests/AppInsightsResourceTests.cs
+++ PulumiDemo/PulumiDemoTests/AppInsightsResourceTests.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Pulumi.AzureNative.OperationalInsights;
 using Pulumi.AzureNative.Resources;
 using Pulumi.Testing;
 using Xunit;
@@ -17,7 +18,10 @@ namespace PulumiDemoTests
 		    var resourceGroups = resources.OfType<ResourceGroup>().ToList();
 		    resourceGroups.Count.Should().Be(1, "a single resource group is expected");
 
-		    var appInsights = resources.OfType<Pulumi.AzureNative.Insights.Component>().ToList();
+		    var workspace = resources.OfType<Workspace>().ToList();
+		    workspace.Count.Should().Be(1, "a single workspace instance is expected");
+		    
+		    var appInsights = resources.OfType<Pulumi.AzureNative.Insights.V20200202.Component>().ToList();
 		    appInsights.Count.Should().Be(1, "a single app insights instance is expected");
 	    }
     }
